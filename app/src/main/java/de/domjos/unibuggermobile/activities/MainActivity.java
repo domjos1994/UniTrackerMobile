014d9e5d3f52c16eb519afa6b357cb1845a9ac32
domjos1994 <developing@domjos.de>
2019-06-02 09:14:51 +0200

    solve bug 0000139

--- app/src/main/java/de/domjos/unibuggermobile/activities/MainActivity.java
+++ app/src/main/java/de/domjos/unibuggermobile/activities/MainActivity.java
@@ -413,6 +413,7 @@ public final class MainActivity extends AbstractActivity implements OnNavigation
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (resultCode == RESULT_OK && requestCode == MainActivity.RELOAD_ACCOUNTS) {
             this.reloadAccounts();
+            this.reloadProjects();
             this.fillFields();
         }
         if (resultCode == RESULT_OK && requestCode == MainActivity.RELOAD_PROJECTS) {
