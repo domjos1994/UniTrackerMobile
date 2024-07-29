ac8bdc23be5c19c79b75394119aba625cfaa37f4
domjos1994 <developing@domjos.de>
2024-07-29 14:58:42 +0200

    update copyright

--- unitrackerlibrary/src/main/java/de/domjos/unitrackerlibrary/custom/RecycleViewHolder.java
+++ unitrackerlibrary/src/main/java/de/domjos/unitrackerlibrary/custom/RecycleViewHolder.java
@@ -1,10 +1,19 @@
 /*
- * Copyright (C) 2017-2020  Dominic Joas
+ * Copyright (C)  2019-2024 Domjos
+ * This file is part of UniTrackerMobile <https://unitrackermobile.de/>.
  *
- *  This program is free software; you can redistribute it and/or
- *  modify it under the terms of the GNU General Public License
- *  as published by the Free Software Foundation; either version 3
- *  of the License, or (at your option) any later version.
+ * UniTrackerMobile is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * UniTrackerMobile is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with UniTrackerMobile. If not, see <http://www.gnu.org/licenses/>.
  */
 
 package de.domjos.unitrackerlibrary.custom;
